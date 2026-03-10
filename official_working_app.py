9820c886e135dad40f8cd7017236ae8e363e3394
admin <admin@admins-MacBook-Pro.local>
2026-03-10 11:45:57 -0700

    Cleaning code

--- official_working_app.py
+++ official_working_app.py
@@ -1810,10 +1810,10 @@ def object_calibration():
 
     # If user uploaded files, use those
     if g_path and r_path:
-        #full_calibration_with_subid(g_path, "wcs_green_solution.fits", os.environ.get("GREEN_SUBID_JUL15")) 
-        #num_rows = full_calibration_with_subid(r_path, "wcs_red_solution.fits", os.environ.get("RED_SUBID_JUL15")) 
-        full_calibration(g_path, "wcs_green_solution.fits")
-        num_rows =full_calibration(r_path, "wcs_red_solution.fits")
+        full_calibration_with_subid(g_path, "wcs_green_solution.fits", os.environ.get("GREEN_SUBID")) 
+        num_rows = full_calibration_with_subid(r_path, "wcs_red_solution.fits", os.environ.get("RED_SUBID")) 
+        #full_calibration(g_path, "wcs_green_solution.fits")
+        #num_rows =full_calibration(r_path, "wcs_red_solution.fits")
 
         # ============================
         # CHECK IF TARGET IS IN IMAGE
